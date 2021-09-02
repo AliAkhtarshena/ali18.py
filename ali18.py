3e1ac62e7ac053c1522d45fda7779d23a1a1ff7e
AliAkhtarshena <89968743+AliAkhtarshena@users.noreply.github.com>
2021-09-02 12:44:46 +0430

    Add files via upload

---
+++ ali18.py
@@ -0,0 +1,103 @@
+# import turtle
+# t = turtle.Pen()
+# t.forward(50)
+# t.forward(50)
+# t.left(90)
+# t.forward(50)
+# t.left(90)
+# t.forward(50)
+
+
+
+
+# import turtle
+# t = turtle.Pen()
+# t.backward(100)
+# t.left(90)
+# t.up()
+# t.forward(30)
+# t.down()
+# t.right(90)
+# t.forward(100)
+# t.hideturtule()
+# t.reset()
+# t.circle(50)
+# t.reset
+# t.color('red')
+# t.circle(50)
+# t.reset()
+# t.fillcolor('yellow')
+# t.begin_fill()
+# t.cicle(50)
+# t.endfill
+
+
+
+
+# import turtle
+# R=turtle.Pen()
+# R.color('Blue')
+# R.forward(60)
+# R.left(90)
+# R.forward(30)
+# R.left(90)
+# R.forward(60)
+# R.left(90)
+# R.forward(30)
+# R.hidetutule()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import turtle
+# c=turtle.Pen()
+# c.fillcolor('blue')
+# c.begin_fill()
+# c.circle(50)
+# c.end_fill()
+# c.fillcolor('yellow')
+# c.begin_fill()
+# c.circle(30)
+# c.end_fill()
+# c.fillcolor('pink')
+# c.begin_fill()
+# c.circle(20)
+# c.end_fill()
+# c.hideturtle()
+
+
+
+import turtle
+boxie=turtle.Pen()
+boxie.up()
+boxie.forward(10)
+boxie.down()
+boxie.forward(40)
+boxie.up()
+boxie.forward(10)
+boxie.left(90)
+boxie.forward(10)
+boxie.down()
+boxie.forward(40)
+boxie.up()
+boxie.forward(10)
+boxie.left(90)
+boxie.forward(10)
+boxie.down()
+boxie.forward(40)
+boxie.up()
+boxie.forward(10)
+boxie.left(90)
+boxie.forward(10)
+boxie.down()
+boxie.forward(40)
+boxie.hideturtle()
